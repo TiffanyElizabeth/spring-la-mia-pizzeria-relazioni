76141b906ac9e799fb77c93e7b19b7349b78bff8
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-06-30 18:35:37 +0200

    add promo codes table to pizza detail page

--- src/main/java/exercises/spring/spring_la_mia_pizzeria_relazioni/repository/PizzaRepository.java
+++ src/main/java/exercises/spring/spring_la_mia_pizzeria_relazioni/repository/PizzaRepository.java
@@ -1,9 +1,13 @@
 package exercises.spring.spring_la_mia_pizzeria_relazioni.repository;
 
 import java.util.List;
+import java.util.Optional;
 import exercises.spring.spring_la_mia_pizzeria_relazioni.model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
     public List<Pizza> findByNameContainingIgnoreCase(String name);
+
 }
