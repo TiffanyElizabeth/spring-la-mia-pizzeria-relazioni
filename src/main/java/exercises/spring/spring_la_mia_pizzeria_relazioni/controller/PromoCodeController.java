4efb4979b7a8e52a773f10caea5590a14f4a6ba9
tiffanybrezak <tiffany.elizabeth.b@gmail.com>
2025-07-02 10:37:59 +0200

    added ability to edit promo codes

--- src/main/java/exercises/spring/spring_la_mia_pizzeria_relazioni/controller/PromoCodeController.java
+++ src/main/java/exercises/spring/spring_la_mia_pizzeria_relazioni/controller/PromoCodeController.java
@@ -11,8 +11,11 @@ import exercises.spring.spring_la_mia_pizzeria_relazioni.repository.PromoCodeRep
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/promos")
@@ -23,6 +26,52 @@ public class PromoCodeController {
 
     @PostMapping("/create")
     public String store(@Valid @ModelAttribute("promoCode") PromoCode formPromoCode, BindingResult bindingResult,
+            Model model) {
+        if (bindingResult.hasErrors()) {
+            return "promo_codes/create";
+        }
+
+        repository.save(formPromoCode);
+
+        return "redirect:/pizzas";
+    }
+
+    // @GetMapping("/edit/{id}")
+    // public String edit(@PathVariable("id") Integer id, Model model) {
+    // PromoCode promoCode = repository.findById(id)
+    // .orElseThrow(() -> new IllegalArgumentException("Invalid promo code ID: " +
+    // id));
+
+    // model.addAttribute("promoCode", promoCode);
+    // model.addAttribute("edit", true);
+    // return "promo_codes/create";
+    // }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        model.addAttribute("promoCode", repository.findById(id).get());
+        model.addAttribute("edit", true);
+        return "promo_codes/create";
+    }
+
+    // @PostMapping("/edit/{id}")
+    // public String update(@PathVariable("id") Integer id, @Valid
+    // @ModelAttribute("promoCode") PromoCode formPromoCode,
+    // BindingResult bindingResult,
+    // Model model) {
+    // if (bindingResult.hasErrors()) {
+    // return "promo_codes/create";
+    // }
+
+    // //formPromoCode.setId(id);
+    // repository.save(formPromoCode);
+
+    // return "redirect:/pizzas";
+    // }
+
+    @PostMapping("/edit/{id}")
+    public String update(@Valid @ModelAttribute("promoCode") PromoCode formPromoCode,
+            BindingResult bindingResult,
             Model model) {
         if (bindingResult.hasErrors()) {
             return "promo_codes/create";
--- src/main/java/exercises/spring/spring_la_mia_pizzeria_relazioni/repository/PromoCodeRepository.java
+++ src/main/java/exercises/spring/spring_la_mia_pizzeria_relazioni/repository/PromoCodeRepository.java
@@ -5,5 +5,4 @@ import exercises.spring.spring_la_mia_pizzeria_relazioni.model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PromoCodeRepository extends JpaRepository<PromoCode, Integer> {
-    // public List<PromoCode> findByNameContainingIgnoreCase(String promoName);
 }
